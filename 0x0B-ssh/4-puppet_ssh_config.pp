2f1b73033ab0d159e0560c49fcb52cd2b4f3ff9f
untalinfo <racso1607@gmail.com>
2020-08-04 18:08:06 +0000

    task advanced added

---
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -0,0 +1,16 @@
+# Puppet ssh configuration server
+# Your SSH client configuration must be configured to use the private key ~/.ssh/holberton
+# Your SSH client configuration must be configured to refuse to authenticate using a password
+
+file_line { 'Turn off passwd auth':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => 'PasswordAuthentication no'
+}
+
+file_line { 'Declare identity file':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => 'IdentityFile ~/.ssh/holberton'
+}
+
