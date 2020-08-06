4d825b9c571983abb2194b4f58f5d7d66ad0ce4a
untalinfo <racso1607@gmail.com>
2020-08-06 03:34:05 +0000

    task puppet added

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,1 +1,23 @@
 # Install Nginx in server whit puppet
+package { 'nginx':
+  ensure => installed,
+  name   => 'nginx',
+}
+
+file { '/var/www/html/index.html':
+  content => 'Holberton School for the win!',
+  path    => '/var/www/html/index.html'
+}
+
+file_line { 'title':
+  ensure   => present,
+  path     => '/etc/nginx/sites-available/default',
+  after    => 'server_name _;',
+  line     => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+  multiple => true
+}
+
+service { 'nginx':
+  ensure  => running,
+  require => Package['nginx'],
+}
